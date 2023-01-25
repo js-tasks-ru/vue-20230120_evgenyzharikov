306b2e80298d4e8a0701a59be6edbad419bcb765
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2023-01-25 12:50:31 +0300

    add counterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,21 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    onButtonClick() {
+      this.counter += 1;
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
