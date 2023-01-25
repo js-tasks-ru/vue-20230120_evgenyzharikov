43a34e2a672589337cd374e26854f8b2582e581e
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2023-01-25 16:07:05 +0300

    add meetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,59 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
 
+import { createApp, defineComponent } from '../03-MarkedList/vendor/vue.esm-browser';
+
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
+// function fetchMeetupById(meetupId) {
+//   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       return response.json().then((error) => {
+//         throw error;
+//       });
+//     }
+//   });
+// }
 
 // Требуется создать Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      checked: 1,
+      meetup: {},
+      loading: false,
+    };
+  },
+  computed: {},
+  watch: {
+    checked(id) {
+      this.fetchMeetupById(id);
+    },
+  },
+  mounted() {
+    this.fetchMeetupById(this.checked);
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      this.loading = true;
+      fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          response.json().then((item) => {
+            (this.meetup = item);
+          });
+          this.loading = false;
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
