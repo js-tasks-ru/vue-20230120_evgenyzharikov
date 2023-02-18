1e3c8204749cf131af3de2ae9dbca21b559d9b7e
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2023-02-18 16:46:00 +0300

    добавил vuecli задачи

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
-  </div>
+  <UiToastList @remove="removeToast($event)" :toasts="Array.from(toasts.values())" />
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
-
+import UiToastList from './UiToastList.vue';
+const defaultTtlMs = 5000;
 export default {
   name: 'TheToaster',
-
-  components: { UiIcon },
+  components: {
+    UiToastList,
+  },
+  data: function() {
+    return {
+      toasts: new Map(),
+    };
+  },
+  lastId: 0,
+  methods: {
+    error: function(message) {
+      this.pushToast('error', message);
+    },
+    success: function(message) {
+      this.pushToast('success', message);
+    },
+    pushToast: function(kind, message, ttl=defaultTtlMs) {
+      const id = this.$options.lastId++;
+      const toast = {
+        id,
+        kind,
+        message,
+        ttl,
+      };
+      this.toasts.set(id, toast);
+      setTimeout(() => {
+        this.removeToast(id);
+      }, ttl);
+    },
+    removeToast: function(id) {
+      this.toasts.delete(id);
+    },
+  },
 };
 </script>
 
