78e791ec8438390bccf9aa531e9e57671830abef
Zharikov.EI <zharikov.ei@gazprom-neft.ru>
2023-02-18 16:07:37 +0300

    поправил CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,7 +3,11 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
   props: {
-    count: { type: Number, default: 0 },
+    count: {
+      type: Number,
+      default: 0,
+      required: true
+    },
   },
   data(){
     return {
@@ -11,24 +15,13 @@ export default defineComponent({
     }
   },
   emits: ['update:count'],
-  watch: {
-    count(value){
-      this.innerCount = value
-    }
-  },
-  computed: {
-    counter: {
-      get() {
-        return this.innerCount
-      },
-      set(value) {
-        this.$emit('update:count', this.innerCount = value)
+    methods: {
+      increment() {
+        this.$emit('update:count', this.count + 1);
       }
-    }
-
-  },
+    },
 
   // Компонент должен иметь входной параметр и порождать событие
 
-  template: `<button type="button" @click="$emit('update:count', counter++)">{{ count }}</button>`,
+  template: `<button type="button" @click="increment()">{{ count }}</button>`,
 });
